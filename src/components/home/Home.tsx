3bdb981f7a20c48e3efcc3672a6add1d89801cd1
DanielJDupont <ddupont2@uwo.ca>
2021-10-29 20:55:45 -0400

    setup

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
